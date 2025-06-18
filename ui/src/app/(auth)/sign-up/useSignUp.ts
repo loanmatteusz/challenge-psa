f2a2ec307f2fc05a7b3eabe74ec0568729a43940
loanmatteusz <loan.matteusz@gmail.com>
2025-06-18 14:44:30 -0300

    style: adjust imports

--- ui/src/app/(auth)/sign-up/useSignUp.ts
+++ ui/src/app/(auth)/sign-up/useSignUp.ts
@@ -4,7 +4,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import { z } from 'zod';
-import { registerUser } from '@/services/auth';
+import { registerUser } from '@/services/auth.service';
 
 
 const formSchema = z.object({
