0e4f933bc5683683c08e9ca34f13cefcf75369ae
loanmatteusz <loan.matteusz@gmail.com>
2025-06-19 01:20:10 -0300

    chore: fix docker compose environment

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    API_URL: process.env.API_URL,
-  }
 };
 
 export default nextConfig;
